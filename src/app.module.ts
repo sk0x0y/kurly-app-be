7c4678aa3ed6ccb38aaeff93d7207da85d69e902
sk0x0y <seokyeong.yang@kakao.com>
2023-03-14 15:04:29 +0900

    chore(develop): apply conditional host environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: (() => {
+        const env = process.env.NODE_ENV;
+
+        if (env === 'production') {
+          return 'localhost';
+        }
+
+        return '49.247.41.209';
+      })(),
+      port: 3306,
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: 'kurly',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
