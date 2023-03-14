1f09b9625156b8b3a458535b33cfa26c4912ac06
sk0x0y <seokyeong.yang@kakao.com>
2023-03-14 15:58:22 +0900

    chore(develop): apply conditonal listning port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(
+    (() => {
+      const env = process.env.NODE_ENV;
+
+      if (env === 'production') {
+        return 5000;
+      }
+
+      return 8000;
+    })(),
+  );
 }
+
 bootstrap();
