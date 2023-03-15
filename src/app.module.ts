46bf763930400b11a6b80b6b44975eef0d0cd0d3
sk0x0y <seokyeong.yang@kakao.com>
2023-03-16 03:58:16 +0900

    chore(develop): remove typeorm, add prisma orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: (() => {
-        const env = process.env.NODE_ENV;
-
-        if (env === 'production') {
-          return 'mariadb';
-        }
-
-        return '49.247.41.209';
-      })(),
-      port: 3306,
-      username: 'root',
-      password: process.env.DB_PASSWORD,
-      database: 'kurly',
-      entities: [],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
